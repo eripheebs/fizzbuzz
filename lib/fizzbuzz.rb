52fd75eab2c225db71ef8ad3191419461d4c0780
eripheebs <erikapheby@gmail.com>
2016-02-23 14:13:21 +0000

    Finished Fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,16 @@
 def fizzbuzz(number)
-  if number % 3 == 0
-    'fizz'
+  if (number % 3 != 0) && (number % 5 != 0)
+  	return number
   end
+
+  string = ''
+  if number % 3 == 0
+    string << 'fizz'
+  end
+
+  if number % 5 == 0
+  	string << 'buzz'
+  end
+
+  return string
 end 
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,4 +6,13 @@ describe 'Fizzbuzz' do
     expect(fizzbuzz(6)).to eq('fizz')
   end
 
+  it 'expect buzz when there is a multiple of 5' do
+  	expect(fizzbuzz(5)).to eq ('buzz')
+  end
+
+  it 'expect fizzbuzz when there is a multiple of 5 and 3' do
+  	expect(fizzbuzz(15)).to eq ('fizzbuzz')
+  end
+
 end
+
